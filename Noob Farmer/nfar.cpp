c3284fb388fe9c6125b9e1d2f431127ddde65097
Bipul Kumar <imbipulkumar@users.noreply.github.com>
2019-09-02 21:22:19 +0530

    Written solution of the problem

--- Noob Farmer/nfar.cpp
+++ Noob Farmer/nfar.cpp
@@ -1,1 +1,32 @@
 //Noob Farmer Problem Code: NFAR
+#include <bits/stdc++.h> 
+  
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long double a,b,c,p,t_area;
+        cin>>a>>b>>c>>p;
+        t_area=(a+b+c)+(a*a*c+a*b*c+a*c*c)/(a*b+b*b-a*c);
+        cout<<fixed<<setprecision(2)<<t_area*p<<endl;
+    }
+    return 0;
+}
+/*
+Sample Input:
+2
+5 4 4
+2
+1 2 1
+5
+Sample Output:
+58.50
+24.00
+*/
